9657d3ca1c9f058f7f6d3e3288ec915bd4761e9d
machine-gun2020 <76742771+machine-gun2020@users.noreply.github.com>
2024-06-18 09:30:04 -0600

    master version

---
+++ src/main/java/com/example/study/StudyApplication.java
@@ -0,0 +1,27 @@
+package com.example.study;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudyApplication.class, args);
+
+		List<Integer> list = Arrays.asList(2,52,3,76,9,4);
+		Integer max = list
+				.stream()
+				.mapToInt(x -> x)
+				.max()
+				.getAsInt();
+			System.out.println(max);
+
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into countries (id, name) values (1,'MEXICO');
+insert into countries (id, name) values (2,'PARAGUAY');
+insert into countries (id, name) values (3,'URUGUAY');
+insert into countries (id, name) values (4,'ARGENTINA');
+insert into countries (id, name) values (5,'BRAZIL');
+insert into countries (id, name) values (6,'PANAMA');
+insert into countries (id, name) values (7,'EL SALVADOR');
+insert into countries (id, name) values (8,'USA');
